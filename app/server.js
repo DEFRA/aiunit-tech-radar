39fcfbbfbf51915425048fd905ceed6d4a9456bb
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-02-08 10:19:14 +0000

    Add router plugin

--- app/server.js
+++ app/server.js
@@ -1,15 +1,24 @@
-require('./insights').setup()
+const config = require('./config')
 const Hapi = require('@hapi/hapi')
 
-const server = Hapi.server({
-  port: process.env.PORT
-})
+const createServer = async () => {
+  const server = Hapi.Server({
+    port: config.port,
+    routes: {
+      validate: {
+        options: {
+          abortEarly: false
+        }
+      }
+    },
+    router: {
+      stripTrailingSlash: true
+    }
+  })
 
-const routes = [].concat(
-  require('./routes/healthy'),
-  require('./routes/healthz')
-)
+  await server.register(require('./plugins/router'))
 
-server.route(routes)
+  return server
+}
 
-module.exports = server
+module.exports = createServer
