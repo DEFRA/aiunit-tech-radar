5d24db2387cd948e011556ad1e737546e2114452
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-02-20 09:00:41 +0000

    Fix linting

--- app/models/create-release.js
+++ app/models/create-release.js
@@ -1,5 +1,5 @@
 class CreateReleaseModel {
-  constructor(year, month) {
+  constructor (year, month) {
     this.year = `${year}`.padStart(4, '0')
     this.month = `${month}`.padStart(2, '0')
   }
